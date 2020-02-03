9c5c1ba05ceda7026b240238a55fe85b8bec2b1b
Yunyoung Chung <double.y.0525@gmail.com>
2020-02-03 11:15:37 +0900

    S02P13A309-23 # footer 추가

---
+++ front-sk/src/components/common/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="footer">
+    <div>ⓒ 2020 SSAFY 2 Seoul class 3 team 9</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#footer {
+  color: white;
+  background: rgb(21, 15, 49);
+  height: 4rem;
+  text-align: center;
+  line-height: 4rem;
+}
+</style>
