f936d71c443003f343454ce8f1e978f6bf9a7d5e
woojae <woojae121@gmail.com>
2020-02-14 17:52:29 +0900

    userprofile fileupload

--- front-sk/src/views/user/Userprofile.vue
+++ front-sk/src/views/user/Userprofile.vue
@@ -10,7 +10,10 @@
             :src="user.image"
             style="position:absoulte; width:100%; height:100%;"
           />
+          <!--file upload component-->
+        
         </div>
+        
         <h1 class="sansfont" style="margin-top:1%;text-align:center">
           {{ user.nickname }}
         </h1>
@@ -18,6 +21,9 @@
           (ID : {{ user.email }})
         </p>
       </div>
+
+           <FileUpload/> 
+
       <v-col class="col-1"></v-col>
       <v-col>
         <div style="display:inline-block; width: 70%">
@@ -148,6 +154,7 @@
 <script>
 import Constant from "../../Constant";
 import { Carousel3d, Slide } from "vue-carousel-3d";
+import FileUpload from '@/components/FileUpload';
 
 export default {
   data: () => {
@@ -206,6 +213,7 @@ export default {
       });
   },
   components: {
+    FileUpload,
     Carousel3d,
     Slide
   },
